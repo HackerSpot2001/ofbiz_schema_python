56894b7b66b6e19b91924def33f8b556d8b215d9
Abhishek Sagar <abhisheksagar513@gmail.com>
2023-07-15 23:17:45 +0530

    registers model in admin.py

--- apps.py
+++ apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class OfbizDjConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ofbiz_dj'
+    name = 'ofbiz_schema_python'
